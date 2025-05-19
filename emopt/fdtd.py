c1d89d0ba5dea90eeb22213e22d47fc749c66b6d
Falk Leonard Ebert <falk.ebert@kip.uni-heidelberg.de>
2025-05-19 14:21:53 +0200

    Fix property getter for wavelength

--- emopt/fdtd.py
+++ emopt/fdtd.py
@@ -442,7 +442,7 @@ class FDTD(MaxwellSolver):
 
     @property
     def wavelength(self):
-        self._wavelength = wavelength
+        return self._wavelength
 
     @wavelength.setter
     def wavelength(self, wlen):
